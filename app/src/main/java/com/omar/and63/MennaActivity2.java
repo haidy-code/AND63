5e3105fee96c4f747c5ce492af75ccb7514401b5
Menna-Elberamy <mennaberam@gmail.com>
2021-03-25 14:35:44 +0200

    color change

---
+++ app/src/main/java/com/omar/and63/MennaActivity2.java
@@ -0,0 +1,14 @@
+package com.omar.and63;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MennaActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menna2);
+    }
+}
